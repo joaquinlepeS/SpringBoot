b97d8b51a6c42880f4d9bdd72b18e21eb4a55a2a
Vina del Mar <vina@duoc.acad>
2025-04-15 09:48:05 -0400

    Primera API

--- HelloWorld/src/main/java/com/HELLOWORLD/ejemplo/HelloWorld/Contoler/HelloControler.java
+++ HelloWorld/src/main/java/com/HELLOWORLD/ejemplo/HelloWorld/Contoler/HelloControler.java
@@ -1,12 +1,14 @@
 package com.HELLOWORLD.ejemplo.HelloWorld.Contoler;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("Api/v1")
 public class HelloControler {
 
-    @GetMapping("/")    
+    @GetMapping("/inicio")
     public String holaMundo(){
         return "Hola Mundo";
     }
